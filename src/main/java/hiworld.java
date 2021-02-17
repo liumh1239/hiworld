bfd08d77cf5e43606d76853e4e04af97c53fed45
liumh <suyuan1239@163.com>
2021-02-17 15:39:05 +0800

    init

--- src/main/java/hiworld.java
+++ src/main/java/hiworld.java
@@ -1,5 +1,7 @@
 public class hiworld {
     public static void main(String[] args) {
         System.out.println("hi world");
+        System.out.println("hi world");
+        System.out.println("hi world");
     }
 }
